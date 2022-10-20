4aedb002d2b0072a11eeeec88dfb108e420acd41
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-10-20 13:21:39 -0300

    Instanciação do Objeto

--- Exercicio-POO/src/application/Program.java
+++ Exercicio-POO/src/application/Program.java
@@ -1,5 +1,28 @@
 package application;
 
-public class Program {
+import entities.Cars;
 
+public class Program {
+	public static void main(String[] args) {
+		Cars carro1 = new Cars(null, null, 0);
+		
+		carro1.setCor("Azul");
+		carro1.setModelo("Civic");
+		carro1.setCapacidadeTanque(59);
+		System.out.println("Carro 1:");
+		System.out.println(carro1.getModelo());
+		System.out.println(carro1.getCor());
+		System.out.println(carro1.getCapacidadeTanque());
+		System.out.println(carro1.totalValorTanque(6.39));
+		
+		System.out.println();
+		Cars carro2 = new Cars("Cinza", "Mercedes-Benz", 66);
+		System.out.println("Carro 2:");
+		System.out.println(carro2.getModelo());
+		System.out.println(carro2.getCor());
+		System.out.println(carro2.getCapacidadeTanque());
+		System.out.println(carro2.totalValorTanque(6.29));
+		
+		
+	}
 }
--- Exercicio-POO/src/entities/Cars.java
+++ Exercicio-POO/src/entities/Cars.java
@@ -2,4 +2,43 @@ package entities;
 
 public class Cars {
 
+	String cor;
+	String modelo;
+	int capacidadeTanque;
+	
+	public Cars(String cor, String modelo, int capacidadeTanque) {
+		this.cor = cor;
+		this.modelo = modelo;
+		this.capacidadeTanque = capacidadeTanque;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+
+	public void setCapacidadeTanque(int capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+
+	public double totalValorTanque(double valorCombustivel) {
+		return capacidadeTanque * valorCombustivel;
+	}
+	
+	
 }

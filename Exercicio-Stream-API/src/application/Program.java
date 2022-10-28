b3876e0cea6ce7cb5296e75e7bccb1824444cce8
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-10-28 10:18:35 -0300

    feat: Resolvendo exercício com o Stream

--- Exercicio-Stream-API/src/application/Program.java
+++ Exercicio-Stream-API/src/application/Program.java
@@ -1,55 +1,24 @@
 package application;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Program {
 
     public static void main(String[] args) {
     	
-    	System.out.println("Ordem Aleatória");
-    	Map<Integer, Contato> agenda = new HashMap<>() {{
-    		put(1, new Contato("Simba", 5555));
-    		put(2, new Contato("Cami", 1111));
-    		put(3, new Contato("Jon", 2222));
-    	}};
+    	List<String> numerosAleatorios = Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
     	
-    	System.out.println(agenda);
-    	for (Map.Entry<Integer, Contato> entry : agenda.entrySet()) {
-			System.out.println(entry.getKey() + " - " + entry.getValue().getNome());
-		}
+    	/*System.out.println("Imprima todos os elementos dessa lista de String:" );
+    	numerosAleatorios.forEach(System.out::println);*/
     	
-    	System.out.println("Ordem número telefone");
-    	//Praticando com classe anônima;
-    	/*Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(new Comparator<Map.Entry<Integer, Contato>>() {
-    		@Override
-    		public int compare(Map.Entry<Integer, Contato> cont1, Map.Entry<Integer, Contato> cont2) {
-    			return Integer.compare(cont1.getValue().getNumero(), cont2.getValue().getNumero());
-    		}
-    	});*/
+    	System.out.println("Pegue os 5 primeiros números e coloque dentro de um Set: ");
     	
-    	/* Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(Comparator.comparing(new Function<Map.Entry<Integer, Contato>, Integer>() {
-
-			@Override
-			public Integer apply(Entry<Integer, Contato> cont) {
-				return cont.getValue().getNumero();
-			}
-		})); */
+    	numerosAleatorios.stream().limit(5).collect(Collectors.toSet()).forEach(System.out::println);
     	
-    	Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(Comparator.comparing(
-    		cont  -> cont.getValue().getNumero()));
-		 
-    	
-    	set.addAll(agenda.entrySet());
-    	for (Map.Entry<Integer, Contato> entry : set) {
-			System.out.println(entry.getKey() + " - " + entry.getValue().getNumero() + ": " + entry.getValue().getNome());
-		}
-    	
-    	
+    	System.out.println("Transforme esta lista de String em uma lista de números interios");
+    	numerosAleatorios.stream().map(Integer::parseInt).collect(Collectors.toList()).forEach(System.out::println);
     }
 }

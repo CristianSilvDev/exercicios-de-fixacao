115d7eb754e7def5351bb58934a47522713eb5cb
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-10-28 11:17:42 -0300

    feat: Resolvendo exercício com Stream pt2

--- Exercicio-Stream-API/src/application/Program.java
+++ Exercicio-Stream-API/src/application/Program.java
@@ -2,8 +2,8 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.function.DoubleConsumer;
+import java.util.function.ToIntFunction;
 
 public class Program {
 
@@ -14,11 +14,30 @@ public class Program {
     	/*System.out.println("Imprima todos os elementos dessa lista de String:" );
     	numerosAleatorios.forEach(System.out::println);*/
     	
-    	System.out.println("Pegue os 5 primeiros números e coloque dentro de um Set: ");
+    	/*System.out.println("Pegue os 5 primeiros números e coloque dentro de um Set: ");
     	
-    	numerosAleatorios.stream().limit(5).collect(Collectors.toSet()).forEach(System.out::println);
+    	numerosAleatorios.stream().limit(5).collect(Collectors.toSet()).forEach(System.out::println);*/
     	
-    	System.out.println("Transforme esta lista de String em uma lista de números interios");
-    	numerosAleatorios.stream().map(Integer::parseInt).collect(Collectors.toList()).forEach(System.out::println);
+    	/*System.out.println("Transforme esta lista de String em uma lista de números interios");
+    	numerosAleatorios.stream().map(Integer::parseInt).collect(Collectors.toList()).forEach(System.out::println);*/
+    	
+    	//Exemplo completo / exemplo simplificado
+    	System.out.println("Pegue os números pares e maiores que 2 e coloque em  uma lista");
+    	/*List <Integer> listParesMaioresQue2 = numerosAleatorios.stream().map(Integer::parseInt).filter(new Predicate<Integer>() {
+    		@Override
+    		public boolean test(Integer i) {
+    			if (i %2 == 0 && i>2) return true;
+    			return false;
+    		}
+		}).collect(Collectors.toList());
+    	System.out.println(listParesMaioresQue2);*/
+    	
+    	/*List <Integer> listParesMaioresQue2 = numerosAleatorios.stream().map(Integer::parseInt).filter(i -> (i % 2 == 0 && i>2))
+    			.collect(Collectors.toList());
+    	System.out.println(listParesMaioresQue2);*/
+    	
+    	System.out.println("Mostre a média dos números: ");
+    	numerosAleatorios.stream().mapToInt(Integer::parseInt).average().ifPresent(System.out::println);
+
     }
 }

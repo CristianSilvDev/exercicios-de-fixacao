0d697310cd687426fd4c374d45c2dce21b9fd8e4
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-10-31 14:52:11 -0300

    feat: Solição com reference method com método não estático

--- Exercicio-Interface-Predicate/src/application/Program.java
+++ Exercicio-Interface-Predicate/src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
     	list.add(new Product("Tablet", 350.50));
     	list.add(new Product("Mouse", 80.90));
     	
-    	list.removeIf(Product::staticProductPredicate);
+    	list.removeIf(Product::nonStaticProductPredicate);
     	
     	list.forEach(System.out::println);
     }
--- Exercicio-Interface-Predicate/src/entities/Product.java
+++ Exercicio-Interface-Predicate/src/entities/Product.java
@@ -32,6 +32,9 @@ public class Product {
 		return p.getPrice() >= 100.00;
 	}
 	
+	public boolean nonStaticProductPredicate() {
+		return price >= 100.00;
+	}
 
 	@Override
 	public String toString() {

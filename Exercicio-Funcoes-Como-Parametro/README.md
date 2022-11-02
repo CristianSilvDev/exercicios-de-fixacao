436c9ac2cb7965b079ec8bd03413d1550a68691a
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-02 20:09:21 -0300

    feat: Implementação das classes e primeira solução do exercício

---
+++ Exercicio-Funcoes-Como-Parametro/README.md
@@ -0,0 +1,4 @@
+# Exercicio - Criando funções que recebem funções como parâmetro
+
+Nesse exercício juntamente com o Professor Nélio Alves pude praticar os conceitos de criação de funções que recebem funções como parâmetro
+
---
+++ Exercicio-Funcoes-Como-Parametro/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import model.services.ProductSservice;
+
+public class Program {
+	
+	
+    public static void main(String[] args) {
+    	
+    	List<Product> list = new ArrayList<>();
+    	
+    	list.add(new Product("TV", 900.00));
+    	list.add(new Product("Notebook", 50.00));
+    	list.add(new Product("Tablet", 350.50));
+    	list.add(new Product("Mouse", 80.90));
+    	
+    	ProductSservice ps = new ProductSservice();
+    	
+    	double sum = ps.filteredSum(list);
+    	
+    	System.out.println("Sum = " + String.format("%.2f", sum));
+    }
+}

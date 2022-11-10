f791d7c8b509a140c800f4b887a0b51996af89df
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-10 16:53:51 -0300

    feat: Busca no Banco de Dados

--- Exercicio-JPA/src/main/java/applicacao/Programa.java
+++ Exercicio-JPA/src/main/java/applicacao/Programa.java
@@ -9,21 +9,18 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
 	
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		System.out.println("Pronto!");
+		Pessoa p = em.find(Pessoa.class, 2);
 		
+		System.out.println(p);
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 		
 		
 	}

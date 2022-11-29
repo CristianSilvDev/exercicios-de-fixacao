855340ee519884b7a5af1091d077055a2a723392
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-29 09:53:14 -0300

    feat: Relacionamento de um para muitos

---
+++ Exercicio-Relacionamento-Um-Para-Muitos/src/oo/composicao/CompraTeste.java
@@ -0,0 +1,14 @@
+package oo.composicao;
+
+public class CompraTeste {
+
+	public static void main(String[] args) {
+		Compra c1 = new Compra();
+		c1.cliente = "Cristian";
+		c1.itens.add(new Item("TV", 3, 2000.00));
+		c1.itens.add(new Item("Borracha", 12, 3.89));
+		
+		System.out.println(c1.itens.size());
+		System.out.println(c1.obterValorTotal());
+	}
+}

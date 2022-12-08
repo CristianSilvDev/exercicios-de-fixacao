7f85e404754109847fe3be0060cc187b5522fc9c
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-08 09:35:26 -0300

    feat: interface function encadeando

--- Exercicio-Fixacao-Lambda-Cod3r/src/Consumidor.java
+++ Exercicio-Fixacao-Lambda-Cod3r/src/Consumidor.java
@@ -9,9 +9,9 @@ public class Consumidor {
 		
 		Produto p1 = new Produto("Caneta", 12.0, 0.09);
 		imprimir.accept(p1);
-		Produto p2 = new Produto("Lapis", 14.0, 0.09);
-		Produto p3 = new Produto("Caderno", 34.0, 0.09);
-		Produto p4 = new Produto("Borracha", 3.0, 0.09);
+		Produto p2 = new Produto("Lapis", 14.0, 0.12);
+		Produto p3 = new Produto("Caderno", 34.0, 0.03);
+		Produto p4 = new Produto("Borracha", 3.0, 0.06);
 		
 		
 		List<Produto> produtos = Arrays.asList(p1, p2, p3, p4);

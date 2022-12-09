33f6dbcae653a97e255b470d8368974b986274a1
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-09 18:26:13 -0300

    feat: usando stream api

--- Exercicio-Cod3r-Stream-API/src/Program.java
+++ Exercicio-Cod3r-Stream-API/src/Program.java
@@ -1,21 +1,28 @@
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		List<String> aprovados = Arrays.asList("Lu", "Ana", "Carka");
+		List<String> aprovados = Arrays.asList("Lu", "Ana", "Carla");
 		
 		System.out.println("Usando o forEach");
 		aprovados.forEach(System.out::println);
 		
 		System.out.println("\nUsando Iterator...");
 		
-		Iterator<String> it = aprovados.iterator();
-		while(it.hasNext()) {
-			System.out.println(it.next());
+		Iterator<String> iterator = aprovados.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
 		}
 		
+		 
+		System.out.println("\nUsando Stream...");
+		Stream<String> stream = aprovados.stream();
+		stream.forEach(System.out::println); // Laço interno!!
+		
 	}
+	
 }

2a39af6d35d1c87e8dc32531a20d798abfdca258
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-11 15:38:47 -0300

    feat: segundo exercício com filter

---
+++ Exercicio-Cod3r-Stream-API/src/Aluno.java
@@ -0,0 +1,12 @@
+
+public class Aluno {
+	final String nome;
+	final double nota;
+	
+	public Aluno(String nome, double nota) {
+		this.nome = nome;
+		this.nota = nota;
+	}
+	
+	
+}
--- Exercicio-Cod3r-Stream-API/src/Filter.java
+++ Exercicio-Cod3r-Stream-API/src/Filter.java
@@ -9,5 +9,19 @@ public class Filter {
 		//.stream() para transformar em Stream
 		// .collect(Collectores.toList()) para transformar em List novamente
 		numeros.stream().filter(s -> s % 2 == 0).collect(Collectors.toList()).forEach(System.out::println);
+		
+		
+		Aluno a1 = new Aluno("Ana", 7.8);
+		Aluno a2 = new Aluno("Bia", 5.8);
+		Aluno a3 = new Aluno("Daniel", 9.8);
+		Aluno a4 = new Aluno("Gui", 6.8);
+		Aluno a5 = new Aluno("Rebeca", 7.1);
+		Aluno a6 = new Aluno("Pedro", 8.8);
+
+		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6);
+		alunos.stream()
+		.filter(a -> a.nota >= 7)
+		.map(a -> "Parabéns " + a.nome + " você foi aprovado")
+		.forEach(System.out::println);
 	}
 }

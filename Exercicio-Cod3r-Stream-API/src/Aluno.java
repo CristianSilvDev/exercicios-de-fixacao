98748c56946c1aeda5255c911036dad1cccd59c4
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-11 20:14:38 -0300

    feat: desafio filter

--- Exercicio-Cod3r-Stream-API/src/Aluno.java
+++ Exercicio-Cod3r-Stream-API/src/Aluno.java
@@ -2,10 +2,12 @@
 public class Aluno {
 	final String nome;
 	final double nota;
+	final boolean comportamento;
 	
-	public Aluno(String nome, double nota) {
+	public Aluno(String nome, double nota, boolean comportamento) {
 		this.nome = nome;
 		this.nota = nota;
+		this.comportamento = comportamento;
 	}
 	
 	
--- Exercicio-Cod3r-Stream-API/src/Filter.java
+++ Exercicio-Cod3r-Stream-API/src/Filter.java
@@ -6,19 +6,19 @@ import java.util.stream.Collectors;
 
 public class Filter {
 	public static void main(String[] args) {
-		List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7);
+		//List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7);
 		
 		//.stream() para transformar em Stream
 		// .collect(Collectores.toList()) para transformar em List novamente
-		numeros.stream().filter(s -> s % 2 == 0).collect(Collectors.toList()).forEach(System.out::println);
+		//numeros.stream().filter(s -> s % 2 == 0).collect(Collectors.toList()).forEach(System.out::println);
 		
 		
-		Aluno a1 = new Aluno("Ana", 7.8);
-		Aluno a2 = new Aluno("Bia", 5.8);
-		Aluno a3 = new Aluno("Daniel", 9.8);
-		Aluno a4 = new Aluno("Gui", 6.8);
-		Aluno a5 = new Aluno("Rebeca", 7.1);
-		Aluno a6 = new Aluno("Pedro", 8.8);
+		Aluno a1 = new Aluno("Ana", 7.8, false);
+		Aluno a2 = new Aluno("Bia", 5.8, true);
+		Aluno a3 = new Aluno("Daniel", 9.8, true);
+		Aluno a4 = new Aluno("Gui", 6.8, false);
+		Aluno a5 = new Aluno("Rebeca", 7.1, true);
+		Aluno a6 = new Aluno("Pedro", 8.8, true);
 
 		// Primeira Maneira
 		/*List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6);
@@ -39,13 +39,14 @@ public class Filter {
 		
 		
 		// Terceira maneira
-		Predicate<Aluno> aprovado = a -> a.nota >= 7;
+		Predicate<Aluno> aprovado = a -> a.nota >= 7 && a.comportamento == true;
+		
 		Function<Aluno, String> saudacaoAprovado = a -> "Parabéns " + a.nome + " você foi aprovado";
 		
 		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6);
 		alunos.stream()
 		.filter(aprovado)
 		.map(saudacaoAprovado)
-		.forEach(System.out::println);
+		.forEach(System.out::println); 
 	}
 }

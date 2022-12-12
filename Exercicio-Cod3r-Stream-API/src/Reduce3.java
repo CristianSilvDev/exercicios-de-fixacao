2659f132f5c932d4e4b1181e59d0050049ecf4e9
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-12 15:39:16 -0300

    feat: utilizando as funções AllMatch, anyMatch e noneMatch

--- Exercicio-Cod3r-Stream-API/src/Reduce3.java
+++ Exercicio-Cod3r-Stream-API/src/Reduce3.java
@@ -7,6 +7,8 @@ import java.util.function.Predicate;
 
 public class Reduce3 {
 	public static void main(String[] args) {
+		
+	
 		Aluno a1 = new Aluno("Ana", 7.1);
 		Aluno a2 = new Aluno("Luna", 6.1);
 		Aluno a3 = new Aluno("Gui", 8.1);

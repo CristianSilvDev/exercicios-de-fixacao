53cee2e82f1ca5893dbe66bdd400492294ce8746
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-12 10:49:18 -0300

    feat: utilizando o Reduce PART 2

--- Exercicio-Cod3r-Stream-API/src/Aluno.java
+++ Exercicio-Cod3r-Stream-API/src/Aluno.java
@@ -4,11 +4,13 @@ public class Aluno {
 	final double nota;
 	final boolean comportamento;
 	
+	public Aluno(String nome, double nota) {
+		this(nome, nota, true);
+	}
+	
 	public Aluno(String nome, double nota, boolean comportamento) {
 		this.nome = nome;
 		this.nota = nota;
 		this.comportamento = comportamento;
 	}
-	
-	
 }

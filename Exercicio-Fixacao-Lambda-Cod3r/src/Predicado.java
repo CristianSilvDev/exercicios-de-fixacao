e5c56b33c949ba455b74b28fa66ee9076d28e0f4
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-14 19:43:37 -0300

    feat: praticando predicate e foreach

--- Exercicio-Fixacao-Lambda-Cod3r/src/Predicado.java
+++ Exercicio-Fixacao-Lambda-Cod3r/src/Predicado.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class Predicado {	
@@ -6,8 +8,20 @@ public class Predicado {
 		Predicate<Produto> isCaro = prod -> (prod.preco * (1 - prod.desconto)) >= 750;
 		
 		Produto produto = new Produto("TV", 3000.00, 0.15);
+		Produto p2 = new Produto("Celular", 1000.00, 0.80);
 		
 		System.out.println(isCaro.test(produto));
+		
+		Predicate<Produto> isBarato = prod -> (prod.preco * (1 - prod.desconto)) <= 300;
+		
+		System.out.println(isBarato.test(p2));
+		
+		List<String> produtoz = Arrays.asList("Carlos", "Marcela", "Bia");
+		
+		produtoz.forEach(a -> System.out.println(a + "!!!"));
+		
+		produtoz.forEach(System.out::println);
+		  
 	}
 
 }
